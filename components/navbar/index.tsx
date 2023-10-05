e844260a0b727c754c23fadb10b2279b29560344
Poxie <poxen12@hotmail.com>
2023-10-05 04:29:40 +0200

    Increased navbar responsiveness

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/assets/icons/Logo";
 
 export default function Navbar() {
     return(
-        <div className="py-6 w-primary max-w-primary mx-auto flex items-center justify-between">
+        <div className="py-6 w-primary max-w-primary mx-auto flex items-center justify-between gap-3 flex-col sm:flex-row">
             <Logo className="w-36" />
             <span className="text-xs text-secondary">
                 Fast and easy CSGO Empire coins to buy and sell.
