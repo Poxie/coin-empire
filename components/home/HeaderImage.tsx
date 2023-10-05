1dd1dc87c8a61b99f8455e7a00ca825c7a49e0b0
Poxie <poxen12@hotmail.com>
2023-10-05 03:58:34 +0200

    Updated header image styles

--- components/home/HeaderImage.tsx
+++ components/home/HeaderImage.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function HeaderImage() {
     return(
-        <div className="pointer-events-none absolute h-[380px] w-full flex items-start">
+        <div className="pointer-events-none absolute h-[310px] w-full flex items-start">
             <Image
-                className="absolute top-0 left-0 min-w-full h-full object-cover opacity-40 blur-sm"
+                className="absolute top-0 left-0 min-w-full h-full object-cover object-top opacity-40 blur-sm"
                 src={'https://i.poxen.dev/xliBOw.png'}
                 width={1920}
                 height={1080}
